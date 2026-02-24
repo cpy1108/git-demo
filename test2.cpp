277562feabd4e4ca6eebc66c82c820ba7ca6c8c0
cpy1108 <2977628807@qq.com>
2026-02-24 23:14:53 +0800

    third

---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    cout<<"Welcome to C++ programming."<<endl;
+    cout<<"This is a new line added to the code."<<endl;
+    return 0;
+}
